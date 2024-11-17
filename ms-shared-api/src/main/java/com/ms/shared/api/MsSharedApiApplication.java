1562680ddb1001a5715cc4f2131ccccfacb52aeb
Diwakar <diwakermani@gmail.com>
2024-11-17 13:50:26 +0530

    structure change

--- ms-shared-api/src/main/java/com/ms/shared/api/MsSharedApiApplication.java
+++ ms-shared-api/src/main/java/com/ms/shared/api/MsSharedApiApplication.java
@@ -1,12 +1,16 @@
 package com.ms.shared.api;
 
-//@SpringBootApplication
+import org.slf4j.MDC;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MsSharedApiApplication {
 
-/*
+
 	public static void main(String[] args) {
 		MDC.put("app.name", "til-shared-api");
-		SpringApplication.run(TilSharedApiApplication.class, args);
+		SpringApplication.run(MsSharedApiApplication.class, args);
 	}
-*/
+
 }
